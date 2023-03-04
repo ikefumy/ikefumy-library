03c2b7d35d4a602a7c694a4622473afac156daf3
ikefumy <ikeke0254@gmail.com>
2023-03-04 10:50:11 +0900

    includeパスを変更

--- verify/aoj/ntl-1-b.cpp
+++ verify/aoj/ntl-1-b.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_B"
 
-#include "/modint/modint.hpp"
+#include "../../modint/modint.hpp"
 
 #define mint modint<1e9+7>
 
